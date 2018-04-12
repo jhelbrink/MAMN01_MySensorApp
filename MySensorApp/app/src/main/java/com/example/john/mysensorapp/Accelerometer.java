63e64feada8dfbfa10107015fb92e1cba0ccca96
annasjovall <anna_sjovall96@hotmail.com>
2018-04-12 13:22:24 +0200

    added (assumed) LowPass filter for accelerometer

--- MySensorApp/app/src/main/java/com/example/john/mysensorapp/Accelerometer.java
+++ MySensorApp/app/src/main/java/com/example/john/mysensorapp/Accelerometer.java
@@ -14,17 +14,19 @@ package com.example.john.mysensorapp;
 
 public class Accelerometer extends AppCompatActivity implements SensorEventListener{
 
-    private TextView mTextMessage;
+
     private TextView xText, yText, zText;
     private Sensor mySensor;
     private SensorManager SM;
+    private float[] lpAcc = new float[3];
+    static final float ALPHA = 0.97f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer);
 
-        mTextMessage = findViewById(R.id.message);
+
         // Create our Sensor Manger
         SM = (SensorManager)getSystemService(SENSOR_SERVICE);
 
@@ -42,14 +44,47 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+
+
+        lpAcc = lowPass(sensorEvent.values.clone(), lpAcc); // not sure if it works as intended.
+
         //format raw input data to only have 1 decimal.
-        xText.setText("X: " + (String.format("%.1f", sensorEvent.values[0] )));
-        yText.setText("Y: " + (String.format("%.1f",sensorEvent.values[1] )));
-        zText.setText("Z: " + (String.format("%.1f",sensorEvent.values[2] )));
+        xText.setText("X: " + (String.format("%.1f", lpAcc[0] )));
+        yText.setText("Y: " + (String.format("%.1f",lpAcc[1] )));
+        zText.setText("Z: " + (String.format("%.1f",lpAcc[2] )));
+
+
+        //changes size of the xyz values when moving the phone.
+        if(sensorEvent.values[0] > 0){
+            xText.setTextSize(38);
+
+        }else{
+            xText.setTextSize(28);
+        }
+        if(sensorEvent.values[1] > 0){
+            yText.setTextSize(38);
+        }else{
+            yText.setTextSize(28);
+        }
+        if(sensorEvent.values[2] > 0){
+            zText.setTextSize(38);
+        }else{
+            zText.setTextSize(28);
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
+
+    //LP filter from moodle.
+    private float[] lowPass( float[] input, float[] output ) {
+        if ( output == null ) return input;
+        for ( int i=0; i<input.length; i++ ) {
+            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+        }
+        return output;
+    }
+
 }
